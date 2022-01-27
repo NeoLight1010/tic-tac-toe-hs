3a04bf9b03d21737e109db49a6bab8dfd847a827
Anthony Suárez <neolight1010@gmail.com>
2022-01-27 08:19:43 -0500

    Initial Render structure.

--- app/Render.hs
+++ app/Render.hs
@@ -1,6 +1,15 @@
-module Render where
+module Render (gameAsPicture) where
 
 import Graphics.Gloss
+import Game
 
-gameAsPicture :: world -> Picture
-gameAsPicture = undefined
+gameAsPicture :: Game -> Picture
+gameAsPicture g = case state g of
+  Playing -> boardAsPlayingPicture (board g)
+  GameOver w -> boardAsGameOverPicture (state g) (board g)
+
+boardAsPlayingPicture :: Board -> Picture
+boardAsPlayingPicture = const Blank
+
+boardAsGameOverPicture :: State -> Board -> Picture
+boardAsGameOverPicture s = const Blank
